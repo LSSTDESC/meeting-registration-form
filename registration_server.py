3f432907d3abcc0061a94669f1ce7f6563c10c37
EiffL <fr.eiffel@gmail.com>
2018-04-01 02:44:27 -0400

    Adds function to see who is registered

--- registration_server.py
+++ registration_server.py
@@ -56,5 +56,4 @@ def register():
 
 if __name__ == '__main__':
     db.create_all()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    print(Participant.query.all())
