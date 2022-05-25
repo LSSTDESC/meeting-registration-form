302a0fdb12eb162d4152708c66cee81b9dbcd449
Joanne Bogart <jrb@slac.stanford.edu>
2022-05-25 12:43:06 -0700

    make email column a unique index

--- registration_server.py
+++ registration_server.py
@@ -18,7 +18,7 @@ class Participant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(200))
     last_name  = db.Column(db.String(200))
-    email = db.Column(db.String(200))
+    email = db.Column(db.String(200), index=True, unique=True)
     affiliation = db.Column(db.String(200))
     in_person = db.Column(db.String(5))
     badge_name = db.Column(db.String(200))
